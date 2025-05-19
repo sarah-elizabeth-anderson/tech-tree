12b841f847632021c4041a301d42a94e19b8922d
sarah-elizabeth-anderson <sarah.elizabeth.anderson.1998@gmail.com>
2025-05-19 15:45:39 -0600

    Add files via upload

---
+++ js/pipeline-data.js
@@ -0,0 +1,14 @@
+const pipelines = [
+    {
+        name: "Libritarian",
+        image: "images/dont-tread-on-me.jpg",
+        ideas: ["Republican Party", "Tory Party", "Ben Shapiro"]
+    },
+    {
+        name: "Manosphere",
+        image: "images/andrew-tate.jpg",
+        ideas: ["Jordan Peterson", "Anti-Communism", "Donald Trump"]
+    }
+];
+
+export default pipelines;
